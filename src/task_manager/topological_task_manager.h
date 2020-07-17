35684dbc1ec1f8863a0f566511ce5ba125996011
aakv <aakv@nomail>
2020-07-17 12:45:47 +0300

    added a test for topological task manager running sequenced tasks

--- src/task_manager/topological_task_manager.h
+++ src/task_manager/topological_task_manager.h
@@ -1,13 +1,15 @@
 #ifndef TASK_MANAGER_TOPOLOGICAL_TASK_MANAGER_H
 #define TASK_MANAGER_TOPOLOGICAL_TASK_MANAGER_H
 
+#include "thread_pool.h"
 #include <vector>
 #include <functional>
 
 class topological_task_manager
 {
 public:
-    topological_task_manager() {
+    topological_task_manager(thread_pool& pool)
+    : task_pool(pool) {
     }
     
     ~topological_task_manager() {
@@ -18,6 +20,9 @@ public:
     
     void run_tasks() {
     }
+
+private:
+    thread_pool& task_pool;
 
 }; // class topological_task_manager
 
--- tests/task_manager_test/topological_task_manager_test.cpp
+++ tests/task_manager_test/topological_task_manager_test.cpp
@@ -1,8 +1,29 @@
 #include <gtest/gtest.h>
+#include <thread_pool.h>
+#include <topological_task_manager.h>
+#include <vector>
+#include <functional>
 
-TEST(topological_task_manager_test, empty_test)
+TEST(topological_task_manager_test, run_sequenced_tasks)
 {
-    EXPECT_TRUE(true);
+    std::mutex result_mutex;
+    std::vector<int> result;
+    
+    auto task_impl = [&result_mutex, &result] (int i) {
+        std::lock_guard<std::mutex> locked(result_mutex);
+        result.push_back(i);
+    };
+
+    thread_pool pool(std::thread::hardware_concurrency());
+    topological_task_manager task_manager(pool);
+    task_manager.add_task(1, std::bind(task_impl, 1), {});
+    task_manager.add_task(2, std::bind(task_impl, 2), {1});
+    task_manager.add_task(3, std::bind(task_impl, 3), {2});
+    task_manager.add_task(4, std::bind(task_impl, 4), {3});
+    task_manager.add_task(5, std::bind(task_impl, 5), {4});
+    task_manager.run_tasks();
+
+    EXPECT_TRUE((result == std::vector{5,4,3,2,1}));
 }
 
 int main(int argc, char *argv[]) {
