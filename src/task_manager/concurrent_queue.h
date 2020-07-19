b5e9060d4964d631b5dc19639407aa4414302363
aakv <aakv@nomail>
2020-07-19 18:17:57 +0300

    minor fixes of concurrent queue

--- src/task_manager/concurrent_queue.h
+++ src/task_manager/concurrent_queue.h
@@ -10,19 +10,19 @@ class concurrent_queue
 {
 public:
     bool empty() const {
-        std::lock_guard<std::mutex> locked(queue_mutex);
+        std::lock_guard locked(queue_mutex);
         return queue_core.empty();
     }
 
     void push(T element) {
-        std::unique_lock<std::mutex> locked(queue_mutex);
+        std::unique_lock locked(queue_mutex);
         queue_core.push_back(std::move(element));
         locked.unlock();
         queue_cond.notify_one();
     }
 
     bool wait_and_pop(T& element) {
-        std::unique_lock<std::mutex> locked(queue_mutex);
+        std::unique_lock locked(queue_mutex);
         queue_cond.wait(locked, [this] {
             return !queue_core.empty();
         });
@@ -32,7 +32,7 @@ public:
     }
 
     bool try_pop(T& element) {
-        std::lock_guard<std::mutex> locked(queue_mutex);
+        std::lock_guard locked(queue_mutex);
         if (!queue_core.empty()) {
             element = std::move(queue_core.front());
             queue_core.pop_front();
@@ -43,8 +43,8 @@ public:
 
 private:
     mutable std::mutex queue_mutex;
-    std::deque<T> queue_core;
     std::condition_variable queue_cond;
+    std::deque<T> queue_core;
 
 }; // class concurrent_queue
 
